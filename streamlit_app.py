3947a3048a2720628153d00e761b85d540bda05f
edwinvalm <141188656+edwinvalm@users.noreply.github.com>
2023-08-01 22:00:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#We'll add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
