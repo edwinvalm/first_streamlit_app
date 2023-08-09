2359eecd343393bd105ba358378d342bd322d153
edwinvalm <141188656+edwinvalm@users.noreply.github.com>
2023-08-08 19:14:44 -0600

    Update streamlit_app.py
    inset into fruit_load_list values

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,7 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit )
 
+# This will not work correctly, but just go with it now for now
+my_cur.execute("inset into fruit_load_list values ('from streamlit')")
+
+
